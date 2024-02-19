060a6eb10e6a7c074db117e9f7dfa0ac9c6303e0
Trigah <ijenrandy2@gmail.com>
2024-02-19 02:15:14 +0100

    Create User Model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    
+    class Role(models.TextChoices):
+        ADMIN = "ADMIN", 'Admin'
+        TEACHER = "TEACHER", 'Teacher'
+        STUDENT = "STUDENT", 'Student'
+
+    base_role = Role.ADMIN        
+
