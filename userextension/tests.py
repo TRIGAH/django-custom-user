9435866944bc9608a46f819bf12d18d4838a559e
Trigah <ijenrandy2@gmail.com>
2024-02-20 16:07:43 +0100

    test for a new user

--- userextension/tests.py
+++ userextension/tests.py
@@ -24,7 +24,19 @@ class UserAccountTests(TestCase):
             db.objects.create_superuser(
                 email='testuser@super.com', user_name='username1', first_name='first_name', password='password', is_staff=False)
             
-            
+
         with self.assertRaises(ValueError):
             db.objects.create_superuser(
                 email='', user_name='username1', first_name='first_name', password='password', is_superuser=True)
+
+
+    def test_new_user(self):
+        db = get_user_model()
+        user = db.objects.create_user(
+            'testuser@user.com', 'username', 'firstname', 'password')
+        self.assertEqual(user.email, 'testuser@user.com')
+        self.assertEqual(user.user_name, 'username')
+        self.assertEqual(user.first_name, 'firstname')
+        self.assertFalse(user.is_superuser)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_active)
