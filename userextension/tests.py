0b39f63fa0d24acb618c7836070a179eec1ee706
Trigah <ijenrandy2@gmail.com>
2024-02-20 15:55:50 +0100

    test for a new superuser

--- userextension/tests.py
+++ userextension/tests.py
@@ -1,5 +1,16 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
 # Create your tests here.
 class UserAccountTests(TestCase):
-   pass
+    def test_new_superuser(self):
+        db = get_user_model()
+        super_user = db.objects.create_superuser(
+            'testuser@super.com', 'username', 'firstname', 'password')
+        self.assertEqual(super_user.email, 'testuser@super.com')
+        self.assertEqual(super_user.user_name, 'username')
+        self.assertEqual(super_user.first_name, 'firstname')
+        self.assertTrue(super_user.is_superuser)
+        self.assertTrue(super_user.is_staff)
+        self.assertTrue(super_user.is_active)
+        self.assertEqual(str(super_user), "username")
